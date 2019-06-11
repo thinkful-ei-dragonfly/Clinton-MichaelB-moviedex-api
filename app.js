e71586cdbfa39aa00718e90578880e58ccced104
Airstryk-1 <cj2027@yahoo.com>
2019-06-11 09:29:06 -0700

    initial wiring

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
 
+const playstore = require('./playstore');
+
+app.get('/apps', (req, res) => {
+  const {sort, genres} = req.query
+
+  console.log(sort);
+
+  res.send(playstore);
+})
 
 app.listen(8000, () => console.log('server listening on 8000'));
